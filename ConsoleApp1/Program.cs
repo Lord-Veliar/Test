5643ff37c7708568c94a86d83f8e806c47a02ef5
Lord-Veliar <programmer.vip.pro@gmail.com>
2023-07-05 01:58:36 +0300

    kk

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -64,7 +64,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                         }
                     }
                 }
-                else
+                else //
                 {
                     break;
                 }
